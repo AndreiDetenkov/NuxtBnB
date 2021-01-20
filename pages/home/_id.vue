0dd85bb675c20b62d585c968772fa3e8ed1a0d4f
AndreiDetenkov <a.detenkov@gmail.com>
2021-01-20 18:41:42 +0600

    creaing dynamic route

---
+++ pages/home/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ home.title }}
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes.json'
+
+export default {
+  data() {
+    return {
+      home: {}
+    }
+  },
+  created() {
+    this.home = homes.find(item => item.objectID === this.$route.params.id)  
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
