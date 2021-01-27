94abf5102948fca46293082b92b9893d6a9ca473
AndreiDetenkov <a.detenkov@gmail.com>
2021-01-27 14:09:38 +0600

    refactoring for speed

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -32,19 +32,20 @@ export default {
         }
     },
     async asyncData({ params, $dataApi, error }) {
-        const homeResponse = await $dataApi.getHome(params.id)
-        if (!homeResponse.ok) return error({ statusCode: homeResponse.status, message: homeResponse.statusText })
 
-        const reviewResponse = await $dataApi.getReviewsByHomeId(params.id)  
-        if (!reviewResponse.ok) return error({ statusCode: reviewResponse.status, message: reviewResponse.statusText })
+        const responses = await Promise.all([
+            $dataApi.getHome(params.id),
+            $dataApi.getReviewsByHomeId(params.id),
+            $dataApi.getUserByHomeId(params.id)
+        ])
 
-        const userResponse = await $dataApi.getUserByHomeId(params.id)  
-        if (!userResponse.ok) return error({ statusCode: userResponse.status, message: userResponse.statusText })
+        const badResponses = responses.find(response => !response.ok)
+        if (badResponses) return error({ statusCode: badResponses.status, message: badResponses.statusText })
 
-        return { 
-          home: homeResponse.json,
-          reviews: reviewResponse.json.hits,
-          user: userResponse.json.hits[0]
+        return {
+          home: responses[0].json,
+          reviews: responses[1].json.hits,
+          user: responses[2].json.hits[0]
         }
     },
     mounted() {
