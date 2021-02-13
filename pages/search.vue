980f6c589ada30f324c83de1a78fd5b6e39dff39
AndreiDetenkov <a.detenkov@gmail.com>
2021-02-13 13:32:30 +0600

    creating a geo search page

--- pages/search.vue
+++ pages/search.vue
@@ -1,9 +1,25 @@
 <template>
-    <div>{{ $route.query }}</div>
+    <div>
+        {{ lat }} / {{ lng }} - {{ label }}<br />
+        <div v-for="home in homes" :key="home.objectID">
+            {{ home.title }}<br />
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'search',
+    watchQuery: ['lat'],
+    async asyncData({ query, $dataApi }) {
+        const data = await $dataApi.getHomesByLocations(query.lat, query.lng)
+
+        return {
+            homes: data.json.hits,
+            label: query.label,
+            lat: query.lat,
+            lng: query.lng,
+        }
+    },
 }
 </script>
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -3,18 +3,24 @@ export default function (context, inject) {
     const apiKey = '094ded0ef6ef8e6a4e1fb0bae796aab7'
     const headers = {
         'X-Algolia-API-Key': apiKey,
-        'X-Algolia-Application-Id': appId
+        'X-Algolia-Application-Id': appId,
     }
 
     inject('dataApi', {
         getHome,
         getReviewsByHomeId,
-        getUserByHomeId
+        getUserByHomeId,
+        getHomesByLocations,
     })
 
     async function getHome(homeId) {
         try {
-            return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, { headers }))
+            return unWrap(
+                await fetch(
+                    `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
+                    { headers }
+                )
+            )
         } catch (error) {
             return getErrorResponse(error)
         }
@@ -22,15 +28,20 @@ export default function (context, inject) {
 
     async function getReviewsByHomeId(homeId) {
         try {
-            return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/reviews/query`, {
-                headers,
-                method: 'POST',
-                body: JSON.stringify({
-                    filters: `homeId: ${homeId}`,
-                    hitsPerPage: 6,
-                    // atributesToHighlight: []
-                })
-            }))
+            return unWrap(
+                await fetch(
+                    `https://${appId}-dsn.algolia.net/1/indexes/reviews/query`,
+                    {
+                        headers,
+                        method: 'POST',
+                        body: JSON.stringify({
+                            filters: `homeId: ${homeId}`,
+                            hitsPerPage: 6,
+                            // atributesToHighlight: []
+                        }),
+                    }
+                )
+            )
         } catch (error) {
             return getErrorResponse(error)
         }
@@ -38,13 +49,40 @@ export default function (context, inject) {
 
     async function getUserByHomeId(homeId) {
         try {
-            return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/users/query`, {
-                headers,
-                method: 'POST',
-                body: JSON.stringify({
-                    filters: `homeId: ${homeId}`
-                })
-            }))
+            return unWrap(
+                await fetch(
+                    `https://${appId}-dsn.algolia.net/1/indexes/users/query`,
+                    {
+                        headers,
+                        method: 'POST',
+                        body: JSON.stringify({
+                            filters: `homeId: ${homeId}`,
+                        }),
+                    }
+                )
+            )
+        } catch (error) {
+            return getErrorResponse(error)
+        }
+    }
+
+    async function getHomesByLocations(lat, lng, radiusInMeters = 1500) {
+        try {
+            return unWrap(
+                await fetch(
+                    `https://${appId}-dsn.algolia.net/1/indexes/homes/query`,
+                    {
+                        headers,
+                        method: 'POST',
+                        body: JSON.stringify({
+                            aroundLatLng: `${lat},${lng}`,
+                            aroundRadius: radiusInMeters,
+                            hitsPerPage: 10,
+                            // atributesToHighlight: [],
+                        }),
+                    }
+                )
+            )
         } catch (error) {
             return getErrorResponse(error)
         }
@@ -61,7 +99,7 @@ export default function (context, inject) {
             ok: false,
             status: 500,
             statusText: error.message,
-            json: {}
+            json: {},
         }
     }
-} 
+}
